4adfb32468c0ac78fdcc9422e9900cdee5bd05ae
JurPri <mail@privalov.info>
2020-05-02 15:07:29 +0300

    add gitlab-runer-register.sh

---
+++ gitlab-runer-register.sh
@@ -0,0 +1,13 @@
+registration_token="XXXXXXXXXXXXXXXXX"
+
+docker exec -it gitlab-runner1 \
+  gitlab-runner register \
+    --non-interactive \
+    --registration-token ${registration_token} \
+    --locked=false \
+    --description docker-stable \
+    --url https://gitlab-web \
+    --executor docker \
+    --docker-image docker:stable \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-network-mode gitlab-network
